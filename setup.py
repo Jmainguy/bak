717f198025f6e3bb3408c065f7fb50ba6fc0643e
Jonathan Seth Mainguy <jon@soh.re>
2022-01-24 11:06:32 -0500

    only publish on releases

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='bak',
-      version='0.4.1',
+      version='0.4.2',
       license='GPLv2',
       description='Backup a file, defaults to same path and .bak',
       author='Jonathan Mainguy',
