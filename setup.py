e39d047cf67f2a705355f145c0e07b72e22d56cd
Jonathan Seth Mainguy <jon@soh.re>
2022-01-24 10:59:49 -0500

    try and use pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='bak',
-      version='0.3',
+      version='0.4.1',
       license='GPLv2',
       description='Backup a file, defaults to same path and .bak',
       author='Jonathan Mainguy',
       author_email='jon@soh.re',
       url='https://github.com/Jmainguy/bak',
       scripts=['bak', 'unbak'],
+      install_requires=[
+          'xattr',
+      ],
      )
